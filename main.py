1e3e129d4f19442e83ac30a9f964be4506d7e6d5
Alden <kinga112@yahoo.com>
2020-02-10 22:49:56 -0500

    changed get_arrival_time() to get_time_until_arrival(). it returns time until next t at Mission Park Stop.

--- main.py
+++ main.py
@@ -2,33 +2,27 @@ import requests
 import time
 from datetime import datetime
 
-def get_arrival_time():
+def get_time_until_arrival():
     resp = requests.get('https://api-v3.mbta.com/predictions?filter[stop]=place-mispk')
-    if resp.status_code != 200:
-        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-    
-    trains = resp.json()['data']
+    mbta = resp.json()['data']
     count=0
     while True:
-        direction = trains[count]['attributes']['direction_id']
-        # 0 for East Bound 1 for West Bound
-        if direction is 0:
-            print(count)
-            arrival_time = resp.json()['data'][count]['attributes']['arrival_time']
-            break
+        direction = mbta[count]['attributes']['direction_id']
+        # 1 for East Bound, 0 for West Bound
+        if direction is 1:
+            arrival_time = mbta[count]['attributes']['arrival_time']
+            arrival_time = arrival_time.split('T')[1].split('-')[0]
+            now = time.strftime('%H:%M:%S')
+            now = datetime.strptime(now, '%H:%M:%S')
+            arrival_time = datetime.strptime(arrival_time, '%H:%M:%S')
+            time_until_arrival = arrival_time - now
+            time_until_arrival.seconds/60
+            if 'day' in str(time_until_arrival):
+                arrival_time = ''
+            else:
+                break
         count+=1
-    
-    arrival_time = arrival_time.split('T')[1].split('-')[0]
-    return arrival_time
-
-
-def get_time_until_arrival():
-    now = time.strftime('%H:%M:%S')
-    now = datetime.strptime(now, '%H:%M:%S')
-    arrival_time = get_arrival_time()
-    arrival_time = datetime.strptime(arrival_time, '%H:%M:%S')
-    time_until_arrival = arrival_time - now
-    time_until_arrival.seconds/60
     return time_until_arrival
+
 
 print(get_time_until_arrival())
