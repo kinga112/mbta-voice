dc61a8e3c03f636ef82b47482c483c17a58bfc80
Alden <kinga112@yahoo.com>
2020-02-11 21:13:40 -0500

    Updates

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import requests
 import time
 from datetime import datetime
 
+
 def get_time_until_arrival():
     # Stop id: place-mispk
     resp = requests.get('https://api-v3.mbta.com/predictions?filter[stop]=place-mispk')
     mbta = resp.json()['data']
-    count=0
+    count = 0
     while True:
         direction = mbta[count]['attributes']['direction_id']
         # 1 for East Bound, 0 for West Bound
@@ -24,15 +25,16 @@ def get_time_until_arrival():
             # can subtract datetimes to get time until arrival
             time_until_arrival = arrival_time - now
             time_until_arrival.seconds/60
-            # when train just arrives and leaves it prints wierd
+            # when train just arrives and leaves, it prints wierd
             # format of data that isnt useful so I ignore that one
             # and get time until next train
             if 'day' in str(time_until_arrival):
                 arrival_time = ''
             else:
                 break
-        count+=1
+        count += 1
     return time_until_arrival
+
 
 if __name__ == "__main__":
     get_time_until_arrival()
